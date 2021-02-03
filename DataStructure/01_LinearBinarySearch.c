e078f91021c3cae86add1e2a72b97e1fe8785031
pawan <pawantripathi19bca124@gmail.com>
2021-02-03 18:48:19 +0530

    succesfull

---
+++ DataStructure/01_LinearBinarySearch.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <conio.h>
+
+// LinearSearch applies on both sorted and unsorted Array //
+            // Function For Linear Search //
+int LinearSearch(int arr[], int size, int element)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == element)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// LinearSearch applies only on sorted Array //
+      // Function For Binary Search //
+int BinarySearch(int arr[], int size, int element)
+{
+    int low, mid, high;
+     low = 0;
+     high = size - 1;
+    // Searching Starts .....
+    while (low <= high)
+    {
+
+        mid = (low + high) / 2;
+        if (arr[mid] == element)
+        {
+            return mid;
+        }
+        if (arr[mid] < element)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+}
+int main()
+{
+    int arr[] = {10,20,30,40,50,60,70,80,90};
+    int size = sizeof(arr) / sizeof(int);
+    int element = 20;
+    int Search = BinarySearch(arr, size, element);
+    printf("the element %d was found at index %d\n", element, Search);
+    return 0;
+};
